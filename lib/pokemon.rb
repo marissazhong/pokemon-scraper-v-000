e4811b73ec8d5d48e4d9813d6888d8a671c0fca2
Marissa Zhong <mwz2101@columbia.edu>
2018-07-03 19:52:55 +0000

    automatically backed up by learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -8,7 +8,7 @@ class Pokemon
   end
   def self.save(name, type, db)
     new_poke = Pokemon.new(name: name, type: type, db: db)
-    new_poke.id = @all.length-1
+    new_poke.id = @all.length
     @all << new_poke
   end
 end
