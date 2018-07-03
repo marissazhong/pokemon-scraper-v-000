ab3606f539643fc4d46e4264ef657f6b99c4c079
Marissa Zhong <mwz2101@columbia.edu>
2018-07-03 19:52:11 +0000

    automatically backed up by learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,11 +1,10 @@
 class Pokemon
   attr_accessor :name, :type, :db, :id
   @all = []
-  def initialize(name:,type:,db:)
+  def initialize(name:,type:,db:, id:0)
     @name = name
     @type = type
     @db = db
-    @id = 0
   end
   def self.save(name, type, db)
     new_poke = Pokemon.new(name: name, type: type, db: db)
